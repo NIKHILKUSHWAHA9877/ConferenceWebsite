30cb77e281b1161cb6c4d5cac4fe2369dac833c6
Nikhil Kushwaha <124434514+NIKHILKUSHWAHA9877@users.noreply.github.com>
2023-05-28 22:55:25 +0530

    Add files via upload

---
+++ hacakathon.js
@@ -0,0 +1,50 @@
+// let changecolor = document.getElementById('purple').addEventListener('click', function change() {
+//     changecolor.body.style.backgroundColor = "purple";
+//   });
+// function changeBackground() {
+//   document.getElementById('purple').body.style.backgroundColor = "purple";
+// }
+
+
+// function changeBackground() {
+//   var bodyElement = document.getElementById("purple");
+//   if (bodyElement) {
+//     bodyid.style.backgroundColor = "purple";
+//   }
+// }
+
+
+// function changeBackground() {
+//   var bodyElement = document.getElementById("gray");
+//   if (bodyElement) {
+//     bodyid.style.backgroundColor = "gray";
+//   }
+// }
+function changeBackground() {
+  var element = document.getElementById("purple");
+  if (element) {
+    bodyid.style.backgroundColor = "purple";
+  }
+}
+
+function changeBackground_2() {
+  var element = document.getElementById("orange");
+  if (element) {
+    bodyid.style.backgroundColor = "orange";
+  }
+}
+
+
+function changeContent() {
+  var element = document.getElementById("red");
+  if (element) {
+    section_main.style.backgroundColor = "lightblue";
+  }
+}
+
+function changeContent_2() {
+  var element = document.getElementById("green");
+  if (element) {
+    section_main.style.backgroundColor = "lightgreen";
+  }
+}
